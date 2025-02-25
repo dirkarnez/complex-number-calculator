6be97480b4044171f921d3e48ce31d4f56139ab2
Dirk Arnez <smalldirkalex@gmail.com>
2025-02-26 04:20:38 +0800

    - update /esm/complex-number.js

--- esm/complex-number.js
+++ esm/complex-number.js
@@ -33,8 +33,8 @@ export class ComplexNumber {
     }
 
     setExponentialFormWithPI(amplitude, phaseWithPIInRadian) { 
-        this.real = [1, 0, -1, 0][phaseWithPIInRadian >= 0 ? (phaseWithPIInRadian / 0.5) % 4 : ((phaseWithPIInRadian/0.5) % 4) + 4]
-            || (amplitude * Math.cos(phaseWithPIInRadian * Math.PI));
+        const index = phaseWithPIInRadian >= 0 ? (phaseWithPIInRadian / 0.5) % 4 : ((phaseWithPIInRadian/0.5) % 4) + 4;
+        this.real = amplitude * (!!index ? [1, 0, -1, 0][index] : Math.cos(phaseWithPIInRadian * Math.PI));
         this.imaginery = amplitude * Math.sin(phaseWithPIInRadian * Math.PI);
     }
 
